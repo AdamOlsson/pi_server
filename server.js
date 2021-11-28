7b20898eed82f9853583002e397eb301a9d45816
AdamOlsson <Olsson.95_@hotmail.com>
2021-11-28 15:54:42 +0100

    simplified server

--- server.js
+++ server.js
@@ -1,27 +1,43 @@
-const http = require('http');
-const fs = require('fs');
-const config = require('./config.json')
-const express = require('express')
+const fs        = require('fs');
+// const config    = require('./config.json;')
+const express   = require('express');
+const csv       = require('csvtojson');
 
 const server = express();
 
 server.use(express.static('public'));
 
-config.projects.forEach(project => {
-    console.log("Creating endpoint " + project.route);
-    server.get(project.route, (req, res) => {
+const port = 3000;
 
-        let rawdata = fs.readFileSync(project.response_file);
-        let jsondata = JSON.parse(rawdata);
+console.log("Creating endpoint " + "/weatherData");
+server.get("/weatherData", (req, res) => {
+    const weather_data_file = "/home/pi/projects/weather/data.json";
+
+    let rawdata = fs.readFileSync(weather_data_file);
+    let jsondata = JSON.parse(rawdata);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', "application/json");
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.end(JSON.stringify(jsondata))
     
-        res.statusCode = 200;
-        res.setHeader('Content-Type', project['content-type']);
-        res.setHeader('Access-Control-Allow-Origin', project['access-control-allow-origin']);
-        res.end(JSON.stringify(jsondata))
-        
+});
+
+console.log("Creating endpoint " + "/cryptoTraderLogs");
+server.get("/cryptoTraderLogs", (req, res) => {
+    const crypto_trader_logs = "/home/adam/Documents/projects/crypto_trader/logs/log.csv"
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', "application/json");
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    
+    csv()
+        .fromFile(crypto_trader_logs)
+        .then(function(jsonArrayObj) {
+            res.end(JSON.stringify(jsonArrayObj));
     })
 });
 
-server.listen(config.port, () => {
-    console.log(`Server running at http://localhost:${config.port}/`);
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
   });
